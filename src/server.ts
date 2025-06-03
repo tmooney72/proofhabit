25eb0ee647a2054d867e94dd78aa52509d697c3c
tylermooney72 <tylermooney32@gmail.com>
2025-06-03 13:59:48 -0500

    chore: install and configure ts-node-dev for development

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('ProofHabit API is running with TypeScript');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
